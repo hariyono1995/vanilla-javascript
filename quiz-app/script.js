af09d58aa0f54440a13e942714a392cc76bca881
hariyono1995 <onnelikrio@gmail.com>
2022-05-14 22:39:07 +0700

    initial commit quiz app

---
+++ quiz-app/script.js
@@ -0,0 +1,22 @@
+const quizData = [
+  {
+    question: "What is the capital of India?",
+    answers: ["New Delhi", "Mumbai", "Chennai", "Kolkata"],
+    correctAnswer: "New Delhi",
+  },
+  {
+    question: "What is the capital of USA?",
+    answers: ["New Delhi", "Mumbai", "Chennai", "Washington, D.C."],
+    correctAnswer: "Washington, D.C.",
+  },
+  {
+    question: "What is the capital of Australia?",
+    answers: ["New Delhi", "Mumbai", "Chennai", "Canberra"],
+    correctAnswer: "Canberra",
+  },
+  {
+    question: "What is the capital of Germany?",
+    answers: ["New Delhi", "Mumbai", "Chennai", "Berlin"],
+    correctAnswer: "Berlin",
+  },
+];
